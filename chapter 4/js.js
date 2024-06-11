62c44ef45a4ee2799aaa6c4e5a8d7bc929ab522a
monu3 <m9800564411@gmail.com>
2024-06-11 08:35:22 +0545

    doing numbers datatype in javascript

---
+++ chapter 4/js.js
@@ -0,0 +1,100 @@
+document.title = " javascript unit 4";
+
+// Variables
+
+console.log(a2);
+// console.log(a);
+let a = "monu"; //doesn't support hoisting
+let roll = 25; //number
+const a1 = "monu";
+const country = "Nepal"; //String
+var a2 = "diwash"; // supporting hoisting
+
+// String
+let abc = "string 1";
+let bc = "string 2";
+let c = `string 3 ${abc}`;
+console.log(c);
+console.log(c + abc);
+
+//trick
+
+let num = 20;
+console.log(num - "20"); // it gives 0 result
+console.log(num + "20"); // it gives concatination of 2020
+
+//Number
+
+let num2 = 20;
+let num3 = 20.02;
+console.log(num2 + num3);
+
+//boolean
+let isMale = true;
+let isHoliday = false;
+//console.log(isHoliday<isMale);
+
+//Function
+
+//Named function
+//with parameter
+function getDay(greetText) {
+  return greetText + "Friday";
+}
+
+let today = getDay("Hi today is ");
+console.log(today);
+
+//with multiple parameter
+function sum(num1, num2) {
+  return num1 + num2;
+}
+let result = sum(30, 30);
+console.log(sum(10, 20) + result);
+
+//Annonymous function
+
+let addition = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(addition(10, 10));
+
+let getSth = () => {
+  return "something";
+};
+console.log(getSth());
+
+let getNth = () => {
+  return sum(100, 100);
+};
+console.log(getNth());
+
+let getNNth = () => {
+  return "My name is smonu siddiki.";
+};
+console.log(getNNth());
+
+
+//Scope 
+
+// let phone = "9876543210";  //global scope
+// function getSth(){
+//     let val ="2345678"; //function scope 
+//     if(1===1){
+//         let val2="dfghjrtyui"; // block scope
+//     }
+//     console.log(val2);
+// }
+// let n = prompt("Enter your number to check : ");
+function checkEven(n){
+    if(n%2===0){
+        console.log(n+" is a even number.");
+    }else{
+        console.log(n + "  is odd number.");
+    }
+}
+
+checkEven(100);
+
+
